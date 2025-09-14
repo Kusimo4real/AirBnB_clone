04f0f81a83fa700769801b8ee2d645bb466f685b
Ask4real <abdulsemiukusimo@gmail.com>
2025-09-14 01:08:47 +0100

    Class Basemodel

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from datetime import datetime
+import uuid
+"""Defines the BaseModel class."""
+
+class BaseModel:
+    """Represents the BaseModel."""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        dict_copy['__class__'] = self.__class__.__name__
+        return dict_copy
